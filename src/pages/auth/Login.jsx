63dfb29d3d931783a9d12e6e1a23f6aaa6a6c4f8
johnsen12 <johnsenultra446@gmai.com>
2024-01-08 23:50:40 +0800

    added auth

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -6,11 +6,51 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import firebaseApp from '../firebaseConfig';
+import { useState } from 'react';
+import Swal from 'sweetalert2'
+
+
 
 const defaultTheme = createTheme();
 
 export default function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  let navigate = useNavigate();
+
+  const handleLogin = () => {
+    if(email !== '' && password !== '') {
+      const auth = getAuth(firebaseApp);
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        navigate('/');
+        // ...
+      })
+      .catch((error) => {
+        Swal.fire({
+          toast: 'true',
+          text: error,
+          icon: 'error',
+          confirmButtonText: 'OK',
+        })
+      });
+    } else {
+      Swal.fire({
+        toast: 'true',
+        text: 'Incorret or missing credentials!',
+        icon: 'error',
+        confirmButtonText: 'OK',
+      })
+    }
+  }
+  
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -40,7 +80,11 @@ export default function Login() {
               label="Email Address"
               name="email"
               autoComplete="email"
-              variant='filled'
+              variant='outlined'
+              onChange={(e) => setEmail(
+                e.target.value
+              )}
+              value={email}
             />
             <TextField
               margin="normal"
@@ -52,9 +96,13 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
-              variant='filled'
+              variant='outlined'
+              onChange={(e) => setPassword(
+                e.target.value
+              )}
+              value={password}
             />
-            <Button type="submit" fullWidth variant="contained" 
+            <Button fullWidth variant="contained" onClick={() => handleLogin()} 
               sx={{ 
                 mt: 3,
                 background: 'black',
