d67a67618cfc4e95c8736cfc24b69ae05ac5cfbe
johnsen12 <johnsenultra446@gmai.com>
2024-01-12 01:38:38 +0800

    change the routing for every component

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,20 @@ function App() {
 
   return (
     <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
-              <Route path='home' element={<Dashboard />} />
-              <Route path='add' element={<AddEmployee />} />
-              <Route path='signup' element={<SignUp />} />
-              <Route path='login'element={<Login />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {/* No need to wrap all routes in Layout */}
+          <Route path="signup" element={<SignUp />} />
+          {/* Login route is not wrapped in a Layout */}
+          <Route path="login" element={<Login />} />
+          {/* Layout will be rendered for these routes */}
+          <Route path="/*" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="home" element={<Dashboard />} />
+            <Route path="add" element={<AddEmployee />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
